8363d39ba4ddcf6ffa3f84d3e0e02b4cf8e983c7
Arman Rostami <arman.rostami.999@gmail.com>
2020-02-07 17:44:41 +0330

    Add Food setter and getters

--- src/main/java/com/loghme/Food/Food.java
+++ src/main/java/com/loghme/Food/Food.java
@@ -12,4 +12,36 @@ public class Food {
         this.popularity = popularity;
         this.price = price;
     }
+
+    String getName() {
+        return name;
+    }
+
+    String getDescription() {
+        return description;
+    }
+
+    double getPopularity() {
+        return popularity;
+    }
+
+    double getPrice() {
+        return price;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    void setDescription(String description) {
+        this.description = description;
+    }
+
+    void setPopularity(double popularity) {
+        this.popularity = popularity;
+    }
+
+    void setPrice(double price) {
+        this.price = price;
+    }
 }
