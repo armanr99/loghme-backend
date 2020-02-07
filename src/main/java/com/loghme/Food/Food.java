3d8e93a5fe6f4438aee6298be4846d99543a9a1e
Arman Rostami <arman.rostami.999@gmail.com>
2020-02-07 17:07:22 +0330

    Add Food class

---
+++ src/main/java/com/loghme/Food/Food.java
@@ -0,0 +1,15 @@
+package com.loghme.Food;
+
+public class Food {
+    private String name;
+    private String description;
+    private double popularity;
+    private double price;
+
+    public Food(String _name, String _description, double _popularity, double _price) {
+        name = _name;
+        description = _description;
+        popularity = _popularity;
+        price = _price;
+    }
+}
