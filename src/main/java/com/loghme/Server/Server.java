9d3c85ede10e6155784e5854bb7c9257775b5825
Parsa Ghorbani <parsa_ghorbani@outlook.com>
2020-02-14 23:44:15 +0330

    Add RestaurantDoesntExist exception handler

--- src/main/java/com/loghme/Server/Server.java
+++ src/main/java/com/loghme/Server/Server.java
@@ -1,10 +1,12 @@
 package com.loghme.Server;
 
 import com.loghme.Constants.Path;
+import com.loghme.Restaurant.Exceptions.RestaurantDoesntExist;
 import com.loghme.User.UserController;
 import com.loghme.Restaurant.RestaurantController;
 import io.javalin.Javalin;
 import com.loghme.Constants.ServerConfigs;
+
 import static io.javalin.apibuilder.ApiBuilder.get;
 
 public class Server {
@@ -22,5 +24,7 @@ public class Server {
             get(Path.Web.RESTAURANTS, RestaurantController.fetchAllRestaurants);
             get(Path.Web.ONE_RESTAURANT, RestaurantController.fetchOneRestaurant);
         });
+
+        app.exception(RestaurantDoesntExist.class, (e, ctx) -> ctx.status(404)).error(404, RestaurantController.restaurantNotFound);
     }
 }
