210aa1b016b746e15f028a72702382360e8b77fc
Parsa Ghorbani <parsa_ghorbani@outlook.com>
2020-02-14 22:37:17 +0330

    Add Path Class constant

--- src/main/java/com/loghme/Constants/Path.java
+++ src/main/java/com/loghme/Constants/Path.java
@@ -3,9 +3,13 @@ package com.loghme.Constants;
 public class Path {
     public static class Web {
         public static final String USER = "/user";
+        public static final String RESTAURANTS = "/restaurants";
+        public static final String ONE_RESTAURANT = "/restaurants/:id";
     }
 
     public static class Template {
         public static final String USER = "/velocity/user/one.vm";
+        public static final String RESTAURANTS_ALL = "/velocity/restaurant/all.vm";
+        public static final String RESTAURANTS_ONE = "/velocity/restaurant/one.vm";
     }
 }
