ecb3ff2ba0430192f6e9e43e26ec8acaf1dac10a
Arman Rostami <arman.rostami.999@gmail.com>
2020-02-14 22:41:02 +0330

    Add view profile

--- src/main/java/com/loghme/User/User.java
+++ src/main/java/com/loghme/User/User.java
@@ -7,16 +7,53 @@ import com.loghme.CartItem.CartItem;
 import com.loghme.Food.Food;
 import com.loghme.Location.Location;
 import com.loghme.Restaurant.Restaurant;
+import com.loghme.Wallet.Wallet;
 
 import java.util.ArrayList;
 
 public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
     private Cart cart;
     private Location location;
+    private Wallet wallet;
 
-    public User() {
-        cart = new Cart();
-        location = new Location(0, 0);
+    public User(int id, String firstName, String lastName, String phoneNumber, String email, Location location, Wallet wallet) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.location = location;
+        this.wallet = wallet;
+        this.cart = new Cart();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public double getCredit() {
+        return wallet.getCredit();
     }
 
     public Location getLocation() {
