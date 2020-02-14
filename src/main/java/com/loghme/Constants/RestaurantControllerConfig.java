b7363a864ca47feeea3424603cf122801909f122
Parsa Ghorbani <parsa_ghorbani@outlook.com>
2020-02-14 23:26:45 +0330

    Add restaurant data url

--- src/main/java/com/loghme/Constants/RestaurantControllerConfig.java
+++ src/main/java/com/loghme/Constants/RestaurantControllerConfig.java
@@ -2,4 +2,5 @@ package com.loghme.Constants;
 
 public class RestaurantControllerConfig {
     public static final double VISIBLE_RESTAURANTS_DISTANCE = 170;
+    public static final String DATA_URL = "http://138.197.181.131:8080/restaurants";
 }
