2d253aaa395a801aba05f61168bcca20a73ec46a
Arman Rostami <arman.rostami.999@gmail.com>
2020-02-21 18:20:49 +0330

    Add get profile servlet

---
+++ src/main/java/com/loghme/controllers/User/UserController.java
@@ -0,0 +1,26 @@
+package com.loghme.controllers.User;
+
+import com.loghme.configs.Path;
+import com.loghme.models.User.User;
+import com.loghme.repositories.UserRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(Path.Web.USER)
+public class UserController extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+
+        User user = UserRepository.getInstance().getUser();
+        request.setAttribute("user", user);
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(Path.jsp.USER);
+        requestDispatcher.forward(request, response);
+    }
+}
