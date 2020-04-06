0aa23dcc13fba086ac0455e9d41fcbd7afe79d5e
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-07 04:00:32 +0430

    Add exception handler and it's wrapper

---
+++ src/main/java/com/loghme/controllers/ExceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.loghme.controllers.ExceptionHandler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler
+{
+}
