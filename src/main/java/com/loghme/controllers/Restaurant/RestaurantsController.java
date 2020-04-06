f2cd9b1236d5fc4014fd34cedbec6741af0810e7
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-07 04:14:28 +0430

    Add get single restaurant service

--- src/main/java/com/loghme/controllers/Restaurant/RestaurantsController.java
+++ src/main/java/com/loghme/controllers/Restaurant/RestaurantsController.java
@@ -2,12 +2,16 @@ package com.loghme.controllers.Restaurant;
 
 import com.loghme.configs.Configs;
 import com.loghme.configs.Path;
+import com.loghme.controllers.Restaurant.wrappers.RestaurantWrapper;
 import com.loghme.controllers.Restaurant.wrappers.RestaurantsWrapper;
 import com.loghme.models.Location.Location;
+import com.loghme.models.Restaurant.Exceptions.RestaurantDoesntExist;
+import com.loghme.models.Restaurant.Exceptions.RestaurantOutOfRange;
 import com.loghme.models.Restaurant.Restaurant;
 import com.loghme.repositories.RestaurantRepository;
 import com.loghme.repositories.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +25,13 @@ public class RestaurantsController {
         Location userLocation = UserRepository.getInstance().getUser().getLocation();
         ArrayList<Restaurant> restaurants = RestaurantRepository.getInstance().getRestaurantsWithinDistance(userLocation, Configs.VISIBLE_RESTAURANTS_DISTANCE);
         return new RestaurantsWrapper(restaurants);
+    }
+
+    @GetMapping("{id}")
+    public RestaurantWrapper getRestaurant(@PathVariable(value = "id") String id) throws RestaurantOutOfRange, RestaurantDoesntExist {
+        Location userLocation = UserRepository.getInstance().getUser().getLocation();
+        RestaurantRepository restaurantRepository = RestaurantRepository.getInstance();
+        Restaurant restaurant = restaurantRepository.getRestaurantInstanceIfInRange(id, userLocation, Configs.VISIBLE_RESTAURANTS_DISTANCE);
+        return new RestaurantWrapper(restaurant);
     }
 }
