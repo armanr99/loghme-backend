03b7c11f1b03379cb1d1b221a09b463b26e2fffa
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-07 12:13:36 +0430

    Add add to cart service

--- src/main/java/com/loghme/controllers/Cart/CartController.java
+++ src/main/java/com/loghme/controllers/Cart/CartController.java
@@ -3,17 +3,30 @@ package com.loghme.controllers.Cart;
 import com.loghme.configs.Path;
 import com.loghme.controllers.wrappers.Cart.CartWrapper;
 import com.loghme.models.Cart.Cart;
+import com.loghme.models.Cart.Exceptions.DifferentRestaurant;
+import com.loghme.models.Food.Exceptions.InvalidCount;
+import com.loghme.models.Restaurant.Exceptions.FoodDoesntExist;
+import com.loghme.models.Restaurant.Exceptions.RestaurantDoesntExist;
+import com.loghme.models.Restaurant.Exceptions.RestaurantOutOfRange;
 import com.loghme.repositories.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(Path.Web.CART)
 public class CartController {
     @GetMapping("")
-    public CartWrapper getUser() {
+    public CartWrapper getCart() {
         Cart cart = UserRepository.getInstance().getUser().getCart();
+        return new CartWrapper(cart);
+    }
+
+    @PostMapping("")
+    public CartWrapper addToCart(@RequestParam("foodName") String foodName,
+                                 @RequestParam("restaurantId") String restaurantId) throws FoodDoesntExist, RestaurantOutOfRange, RestaurantDoesntExist, DifferentRestaurant, InvalidCount {
+        Cart cart = UserRepository.getInstance().getUser().getCart();
+
+        UserRepository.getInstance().addToCart(foodName, restaurantId);
+
         return new CartWrapper(cart);
     }
 }
