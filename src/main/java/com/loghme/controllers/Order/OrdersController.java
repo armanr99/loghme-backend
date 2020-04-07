fc5859fc4d05b31e06ab4d23c3215296fa6ed9c9
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-07 13:06:34 +0430

    Add get single order service

--- src/main/java/com/loghme/controllers/Order/OrdersController.java
+++ src/main/java/com/loghme/controllers/Order/OrdersController.java
@@ -1,12 +1,15 @@
 package com.loghme.controllers.Order;
 
 import com.loghme.configs.Path;
+import com.loghme.controllers.wrappers.Order.OrderWrapper;
 import com.loghme.controllers.wrappers.Order.OrdersWrapper;
 import com.loghme.controllers.wrappers.User.UserWrapper;
 import com.loghme.models.Order.Order;
+import com.loghme.models.User.Exceptions.OrderDoesntExist;
 import com.loghme.models.User.User;
 import com.loghme.repositories.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +22,11 @@ public class OrdersController {
     public OrdersWrapper getOrders() {
         ArrayList<Order> orders = UserRepository.getInstance().getUser().getOrdersList();
         return new OrdersWrapper(orders);
+    }
+
+    @GetMapping("{id}")
+    public OrderWrapper getOrder(@PathVariable(value = "id") String id) throws OrderDoesntExist {
+        Order order = UserRepository.getInstance().getOrder(id);
+        return new OrderWrapper(order);
     }
 }
