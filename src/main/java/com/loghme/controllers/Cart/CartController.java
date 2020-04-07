d62c193ea48ab1b89badfc572b3f293c774a161a
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-07 12:38:00 +0430

    Add delete from cart service

--- src/main/java/com/loghme/controllers/Cart/CartController.java
+++ src/main/java/com/loghme/controllers/Cart/CartController.java
@@ -3,6 +3,7 @@ package com.loghme.controllers.Cart;
 import com.loghme.configs.Path;
 import com.loghme.controllers.wrappers.Cart.CartWrapper;
 import com.loghme.models.Cart.Cart;
+import com.loghme.models.Cart.Exceptions.CartItemDoesntExist;
 import com.loghme.models.Cart.Exceptions.DifferentRestaurant;
 import com.loghme.models.Food.Exceptions.InvalidCount;
 import com.loghme.models.Restaurant.Exceptions.FoodDoesntExist;
@@ -23,9 +24,19 @@ public class CartController {
     @PostMapping("")
     public CartWrapper addToCart(@RequestParam("foodName") String foodName,
                                  @RequestParam("restaurantId") String restaurantId) throws FoodDoesntExist, RestaurantOutOfRange, RestaurantDoesntExist, DifferentRestaurant, InvalidCount {
+        UserRepository.getInstance().addToCart(foodName, restaurantId);
+
         Cart cart = UserRepository.getInstance().getUser().getCart();
 
-        UserRepository.getInstance().addToCart(foodName, restaurantId);
+        return new CartWrapper(cart);
+    }
+
+    @DeleteMapping("")
+    public CartWrapper removeFromCart(@RequestParam("foodName") String foodName,
+                                      @RequestParam("restaurantId") String restaurantId) throws CartItemDoesntExist {
+        UserRepository.getInstance().removeFromCart(foodName, restaurantId);
+
+        Cart cart = UserRepository.getInstance().getUser().getCart();
 
         return new CartWrapper(cart);
     }
