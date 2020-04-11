9ddc4326c7b0200aaee766b1530906a3bae89bb7
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-12 01:27:24 +0430

    Improve cart response

--- src/main/java/com/loghme/controllers/wrappers/responses/Cart/CartItemResponse.java
+++ src/main/java/com/loghme/controllers/wrappers/responses/Cart/CartItemResponse.java
@@ -1,17 +1,17 @@
 package com.loghme.controllers.wrappers.responses.Cart;
 
-import com.loghme.controllers.wrappers.responses.Restaurant.RestaurantResponse;
+import com.loghme.controllers.wrappers.responses.Restaurant.RestaurantSmallResponse;
 import com.loghme.models.CartItem.CartItem;
 import com.loghme.models.Food.Food;
 
 public class CartItemResponse {
     private Food food;
-    private RestaurantResponse restaurant;
+    private RestaurantSmallResponse restaurant;
     private int count;
 
     public CartItemResponse(CartItem cartItem) {
         this.food = cartItem.getFood();
-        this.restaurant = new RestaurantResponse(cartItem.getRestaurant());
+        this.restaurant = new RestaurantSmallResponse(cartItem.getRestaurant());
         this.count = cartItem.getCount();
     }
 }
