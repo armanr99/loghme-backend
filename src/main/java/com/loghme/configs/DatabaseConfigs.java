263ff28e3adc8283f5db78f23bd1c9442a0ed61f
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-26 01:36:17 +0430

    Improve connection configs

--- src/main/java/com/loghme/configs/DatabaseConfigs.java
+++ src/main/java/com/loghme/configs/DatabaseConfigs.java
@@ -1,11 +1,11 @@
 package com.loghme.configs;
 
 public class DatabaseConfigs {
-    public static String driverClassName = "com.mysql.jdbc.Driver";
-    public static String url = "jdbc:mysql://localhost:3306/loghme";
+    public static String driverClassName = "com.mysql.cj.jdbc.Driver";
+    public static String url = "jdbc:mysql://localhost:3306/loghme?useUnicode=yes&characterEncoding=UTF-8";
     public static String username = "root";
     public static String password = "g;QK\"7MP";
-    public static int minIdle = 5;
-    public static int maxIdle = 100;
+    public static int minIdle = 50;
+    public static int maxIdle = 250;
     public static int maxOpenPreparedStatements = 500;
 }
